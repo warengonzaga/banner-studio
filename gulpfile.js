a62a1b9c7f7ed5ee618ab49018afaa6dc7eed20b
Waren Gonzaga <emiren.warengonzaga.official@gmail.com>
2016-11-01 13:26:01 +0000

    Fix the gulp error

--- gulpfile.js
+++ gulpfile.js
@@ -39,79 +39,8 @@ var paths = {
 	//zipMaker paths
 	zipMakerToolInput: 'tools/zipMaker/input/*',
 	zipMakerToolOutput: 'tools/zipMaker/output',
-
-	//bannerTemplate paths
-	bannerTemplate: 'banners/source/template',
-	bannerProduction: 'banners/production'
+	
 };
-
-/*var server = {
-  host: 'localhost',
-  port: '8001'
-}
-
-gulp.task('openbrowser', function() {
-  opn( 'http://' + server.host + ':' + server.port );
-});
-
-gulp.task('webserver', function() {
-  gulp.src( '.' )
-    .pipe(webserver({
-      host:             server.host,
-      port:             server.port,
-      livereload:       true,
-      directoryListing: false
-    }));
-});*/
-
-//***** Banners Task *****//
-var bannerData = JSON.parse(fs.readFileSync('banners/source/settings/banner-properties.json'));
-var dimensions = "";
-
-gulp.task('createBanners', 'Generate banners', function() {
-
-	runSeq('createSizes', 'injectProperties');
-
-});
-
-gulp.task('createSizes', false, ()=> {
-	for (i = 0; i < bannerData.bannerProperties.dimensions.length; i++) { 
-		dimensions = bannerData.bannerProperties.dimensions[i];
-
-	gulp.src(paths.bannerTemplate+'/**/*')
-		.pipe(gulp.dest(paths.bannerProduction+'/'+dimensions));
-
-	}
-})
-
-gulp.task('injectProperties', false, ()=> {
-	for (i = 0; i < bannerData.bannerProperties.dimensions.length; i++) { 
-		dimensions = bannerData.bannerProperties.dimensions[i];
-
-	var bannerwidth = dimensions.slice(0,3);
-	var bannerheight = dimensions.slice(4);
-
-	gulp.src(paths.bannerProduction+'/'+dimensions+'/css/_properties.scss')
-		.pipe(insert.append('$banner-width: '+bannerwidth+'px;\n'))
-		.pipe(insert.append('$banner-width: '+bannerheight+'px;'))
-		.pipe(gulp.dest(paths.bannerProduction+'/'+dimensions+'/css'))
-	}
-})
-
-
-
-/*gulp.task('img', false, ()=> {
-	img.pipe(gulp.dest(paths.bannerProduction+'/'+dimensions+'/img'))
-})
-
-gulp.task('sass', false, ()=> {
-	var bannerwidth = dimensions.slice(0,3);
-	var bannerheight = dimensions.slice(4);
-
-	propertiesScss.pipe(insert.append('$banner-width: '+bannerwidth+'px;\n'))
-			 	  .pipe(insert.append('$banner-width: '+bannerheight+'px;'))
-				  .pipe(gulp.dest(paths.bannerProduction+'/'+dimensions+'/css'))
-});*/
 
 //***** Tools Tasks *****//
 
